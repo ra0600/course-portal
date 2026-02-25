71f98159ddf6bfeb02991ee7602c4b1d18fbedc6
ra0600 <ifrahfathima93@gmail.com>
2026-02-25 19:59:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Course Portal")
+
+st.title("Welcome to My Course Portal")
+
+st.write("This portal will contain modules, videos, activities and resources.")
+
+st.header("Announcements")
+st.write("Course will begin next week.")
+
+st.header("Module Section")
+st.write("Module materials will be uploaded soon.")
+
+st.header("Video Section")
+st.write("Video links will be added soon.")
+
+st.header("Activity Section")
+st.write("Activity details will be updated soon.")
